d2e8b67f2f0f1eec0f2a4f9802c35f554692feb7
601-20 <601-20@DESKTOP-F1CHVJD>
2024-01-15 14:41:02 +0900

    com

--- src/Grade.java
+++ src/Grade.java
@@ -29,7 +29,7 @@ public class Grade {
 
     public static void main(String[] args) {
         Grade grade = new Grade();
-
+        System.out.println("안녕");
         System.out.println("값확인 " + grade.eng + grade.math + grade.kor);
         grade.initGrade(90, 70, 60);
 
