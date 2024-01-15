cbe973ec3220a8df91e274662a7b06d77ba94558
Wolsa1 <156637942+Wolsa1@users.noreply.github.com>
2024-01-15 15:15:04 +0900

    Update AccessWay.java
    커밋7

--- src/AccessWay.java
+++ src/AccessWay.java
@@ -1,7 +1,7 @@
 class AccessWay {
    static int num = 0;
 
-   AccessWay() { incrCnt(); }ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
+   AccessWay() { incrCnt(); }djkgnowngldfsjkl;agjijhklar;
    void incrCnt() {
       num++;   // 클래스 내부에서 이름을 통한 접근
    }
