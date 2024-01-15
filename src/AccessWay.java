4da82972b1e4a459981fcf714e2bb6987bda7bb8
601-20 <601-20@DESKTOP-F1CHVJD>
2024-01-15 15:11:11 +0900

    커밋 5555

--- src/AccessWay.java
+++ src/AccessWay.java
@@ -12,6 +12,6 @@ class ClassVarAccess {
       AccessWay way = new AccessWay();
       way.num++;    // 외부에서 인스턴스의 이름을 통한 접근
       AccessWay.num++;    // 외부에서 클래스의 이름을 통한 접근
-      System.out.println("num = " + AccessWay.num);
+      System.out.println("num = " + AccessWay.num);//
    }
 }
